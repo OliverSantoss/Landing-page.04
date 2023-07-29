2455f42ea149461b2932fdc90ad4ae38d5b330a6
Vinicius Oliveira <106452084+OliverSantoss@users.noreply.github.com>
2023-07-28 20:54:16 -0400

    subindo imgens e edição do slide

--- js/function.js
+++ js/function.js
@@ -11,4 +11,38 @@ $(function () {
         $('html,body').animate({'scrollTop':offsetTop});
         return false;
     });
+
+    $('.mosaico-wraper').slick({
+        centerMode: false,
+        slidesToShow: 6,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 3,
+                    arrows: false
+                }
+            },
+
+            {
+                breakpoint: 580,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 2,
+                    arrows: false
+                }
+            },
+
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    slidesToShow: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
 });
